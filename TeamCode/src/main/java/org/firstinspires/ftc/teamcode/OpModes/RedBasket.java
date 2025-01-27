7bbecd5cd817ebcf388262d2bd3a99e537eeb2c9
Rowan Shenoy <rowans@hotmail.com>
2025-01-27 15:19:35 -0500

    Adds start of code for red bucket auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/RedBasket.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/RedBasket.java
@@ -29,157 +29,13 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.SensorBNO055IMU;
 import org.firstinspires.ftc.teamcode.SparkFunOTOSDrive;
 
-@Disabled
-@Autonomous (name = "DO NOT USE")
+@Autonomous (name = "Red Bucket Testing")
 public final class RedBasket extends LinearOpMode {
     DistanceSensor leftSensor;
     BNO055IMU imu;
     private DcMotorEx lift;
     private Servo claw;
     private Servo pivot;
-
-    /*public class Lift {
-        private DcMotorEx lift;
-
-        public Lift (HardwareMap hardwareMap) {
-            lift = hardwareMap.get(DcMotorEx.class, "arm");
-            lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        }
-
-        public class LiftSetPos implements Action {
-            private boolean initialized = false;
-            private int target;
-            /*
-            Top Basket: -5400
-            Bottom Basket: -2800
-            Top Chamber: -3900
-            Bottom Chamber: -2200
-            Bottom of the lift: 0
-
-
-            public LiftSetPos(int target) {
-                this.target = target;
-            }
-
-            public boolean run(@NonNull TelemetryPacket packet) {
-                initialized = true;
-                lift.setTargetPosition(target);
-                lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                lift.setPower(1);
-
-                return true;
-            }
-        }
-    }
-    */
-
-    /*public class Pivot {
-        private Servo pivot;
-
-        public Pivot (HardwareMap hardwareMap) {
-            pivot = hardwareMap.get(Servo.class, "pivotServo");
-        }
-
-        /*
-            Up: 0.45
-            Chamber: 0.285
-            Down: 0.08
-
-
-        public class PivotPos implements Action {
-            private boolean initialized = false;
-            private double target;
-
-            public PivotPos(double target) {
-                this.target = target;
-            }
-
-            public boolean run(@NonNull TelemetryPacket packet) {
-                if (!initialized) {
-                    initialized = true;
-                    pivot.setPosition(target);
-                }
-
-                return true;
-            }
-        }
-
-        public class PivotUp implements Action {
-            private boolean initialized = false;
-
-
-            public boolean run(@NonNull TelemetryPacket packet) {
-                if (!initialized) {
-                    initialized = true;
-                    pivot.setPosition(0.45);
-                }
-
-                return true;
-            }
-        }
-
-        public class PivotChamber implements Action {
-            private boolean initialized = false;
-
-            public boolean run(@NonNull TelemetryPacket packet) {
-                if (!initialized) {
-                    initialized = true;
-                    pivot.setPosition(0.285);
-                }
-
-                return true;
-            }
-        }
-
-        public class PivotDown implements Action {
-            private boolean initialized = false;
-
-            public boolean run(@NonNull TelemetryPacket packet) {
-                if (!initialized) {
-                    initialized = true;
-                    pivot.setPosition(0.08);
-                }
-
-                return true;
-            }
-        }
-    }*/
-
-    /*public class Claw {
-
-
-        public Claw (HardwareMap hardwareMap) {
-            claw = hardwareMap.get(Servo.class, "claw");
-        }
-
-        public class ClawOpen implements Action {
-            private boolean initialized = false;
-
-            public boolean run(TelemetryPacket packet) {
-                if (!initialized) {
-                    initialized = true;
-                    claw.setPosition(0);
-                }
-
-                return true;
-            }
-        }
-
-        public class ClawClose implements Action {
-            private boolean initialized = false;
-
-            public boolean run(@NonNull TelemetryPacket packet) {
-                if (!initialized) {
-                    initialized = true;
-                    claw.setPosition(1);
-                }
-
-                return true;
-            }
-        }
-    }*/
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -191,117 +47,38 @@ public final class RedBasket extends LinearOpMode {
         parameters.loggingTag          = "IMU";
         parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
 
-        leftSensor = hardwareMap.get(DistanceSensor.class, "left");
+//        leftSensor = hardwareMap.get(DistanceSensor.class, "left");
 
-        claw = hardwareMap.get(Servo.class, "claw");
+//        claw = hardwareMap.get(Servo.class, "claw");
 
-        pivot = hardwareMap.get(Servo.class, "pivotServo");
+//        pivot = hardwareMap.get(Servo.class, "pivotServo");
 
-        lift = hardwareMap.get(DcMotorEx.class, "arm");
-        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        lift = hardwareMap.get(DcMotorEx.class, "arm");
+//        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-
-//        imu = hardwareMap.get(BNO055IMU.class, "imu");
-
-//        imu.initialize(parameters);
-
-//        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-
-        telemetry.addData("X Position", -(72 - (leftSensor.getDistance(DistanceUnit.INCH) + 9)));
-        telemetry.update();
+//        telemetry.addData("X Position", -(72 - (leftSensor.getDistance(DistanceUnit.INCH) + 9)));
 
 //        Pose2d beginPose = new Pose2d(-34, -62, Math.toRadians(90));
-        Pose2d beginPose = new Pose2d(-34, -62, Math.toRadians(90));
+        Pose2d beginPose = new Pose2d(-9.75, -62, Math.toRadians(90));
         Pose2d currentPose = beginPose;
         SparkFunOTOSDrive drive = new SparkFunOTOSDrive(hardwareMap, beginPose);
 
-//        Lift lift = new Lift(hardwareMap);
-//        Pivot pivot = new Pivot(hardwareMap);
-//        Claw claw = new Claw(hardwareMap);
-//
-//        //claw close
-//        Actions.runBlocking(claw. new ClawClose());
-
-        claw.setPosition(1);
-        pivot.setPosition(0.027);
+        telemetry.addLine("ready");
+        telemetry.update();
 
         waitForStart();
 
-        lift.setTargetPosition(-5400);
-        lift.setPower(1);
-        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
         Actions.runBlocking(
-                new SequentialAction(
-                        drive.actionBuilder(beginPose)
-                                //lift up to top basket
-                                .splineTo(new Vector2d(-50, -54.5), Math.toRadians(215))
-                                .lineToX(-56)
-                                //claw open
-                                .build()
-                )
+                drive.actionBuilder(beginPose)
+                        .splineTo(new Vector2d(-9.75, -28), Math.toRadians(90))
+                        .waitSeconds(1.5)
+                        .strafeTo(new Vector2d(-20, -45))
+                        .strafeTo(new Vector2d(-46, -35))
+                        .strafeToSplineHeading(new Vector2d(-57.4, -55), Math.toRadians(225))
+                        .strafeToSplineHeading(new Vector2d(-55, -35), Math.toRadians(90))
+                        .build()
         );
-
-        while(lift.isBusy()){
-            sleep(5);
-        }
-
-        pivot.setPosition(0.285);
-
-        claw.setPosition(0);
-
-        Actions.runBlocking(
-                new SequentialAction(
-                        drive.actionBuilder(new Pose2d(-56, -54.5, Math.toRadians(215)))
-                                .splineTo(new Vector2d(-45, 0), Math.toRadians(180))
-                                .lineToX(-26)
-                                .build()
-                )
-        );
-
-//        claw. new ClawOpen();
-//                        claw. new ClawOpen(),
-//                        drive.actionBuilder(new Pose2d(-57.4, -55, Math.toRadians(225)))
-//                                .turn(Math.atan((-57.4+47.7)/(-55+40))-Math.toRadians(15))
-//                                //lift down
-//                                .build(),
-////                        lift. new LiftSetPos(0),
-//                        drive.actionBuilder(new Pose2d(-57.4, -55, 225 + (Math.atan((-57.4+47.7)/(-55+40))-Math.toRadians(15))))
-//                                .lineToYLinearHeading(-40, Math.toRadians(90))
-//                                .build(),
-////                        pivot. new PivotDown(),
-////                        claw. new ClawClose(),
-//                        drive.actionBuilder( new Pose2d(new Vector2d(-47.7, -40), Math.toRadians(90)))
-//                                .build()
-
-                        //close claw
-                        //pivot up
-                        //lift up to top basket
-//                        .splineToSplineHeading(new Pose2d(-57.4, -55, Math.toRadians(225)), Math.toRadians(225))
-//                        //claw open
-//                        //lift down
-//                        .splineTo(new Vector2d(-57.4, -40), Math.toRadians(90))
-//                        //pivot down
-//                        .waitSeconds(1.5)
-//                        //close claw
-//                        //pivot up
-//                        //lift up to top basket
-//                        .splineTo(new Vector2d(-57.4, -55), Math.toRadians(225))
-//                        //claw open
-//                        //lift down
-//                        .splineTo(new Vector2d(-55, -24), Math.toRadians(180))
-//                        //pivot down
-//                        .waitSeconds(1.5)
-//                        //close claw
-//                        //pivot up
-//                        //lift up to top basket
-//                        .splineTo(new Vector2d(-57.4, -55), Math.toRadians(225))
-//                        //claw open
-//                        //lift down
-//                        .lineToY(-40)
-//                        .splineTo(new Vector2d(-45, 0), Math.toRadians(180))
-//                        .lineToX(-26)
     }
 }
